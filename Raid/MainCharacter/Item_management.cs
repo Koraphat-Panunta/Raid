808964aac7baaf06b3ea7c8bfa356d172a8fb8a3
Koraphat-Panunta <mark_nanza@hotmail.com>
2023-09-12 04:52:33 +0700

    12/9/66 0452

--- Raid/MainCharacter/Item_management.cs
+++ Raid/MainCharacter/Item_management.cs
@@ -12,9 +12,11 @@ namespace Raid.MainCharacter
         public float Max_weight;
         public float carry_weight;
         public float carry_value;
-        public int Grace_num;
-        private Rune_ATK rune_atk = new Rune_ATK();
+        public int Grace_num;        
         public List<Rune_ATK> Rune_ATK = new List<Rune_ATK>();
+        public List<Rune_Armor> Rune_Armor = new List<Rune_Armor>();
+        public List<Rune_Time> Rune_Times = new List<Rune_Time>();
+        public List<Rune_Life> Rune_Lives = new List<Rune_Life>();
         public List<Grace> Graces = new List<Grace>();
         public Inventory(float weight)
         {
@@ -23,7 +25,7 @@ namespace Raid.MainCharacter
         }
         public void Cal_Weight() 
         {
-            carry_weight = Rune_ATK.Count * Rune_ATK[0].Get_Weight() + Graces.Count*Graces[0].Get_Weight(); ;          
+            carry_weight = (Rune_ATK.Count * Rune_ATK[0].Get_Weight() + (Rune_Armor.Count * Rune_Armor[0].Get_Weight()) + (Rune_Times.Count * Rune_Times[0].Get_Weight()) + (Rune_Lives.Count * Rune_Lives[0].Get_Weight()) + Graces.Count * Graces[0].Get_Weight());      
         }
         
     }
